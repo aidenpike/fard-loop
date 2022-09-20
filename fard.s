a44c6532c228335307c0498ab9c21103ec9485b3
Kryllyx <97801783+kryllyxofficial01@users.noreply.github.com>
2022-09-20 13:57:56 -0400

    formatting

--- fard.s
+++ fard.s
@@ -1,14 +1,14 @@
 section .text
 	global _start
 	
-	_start:
-		mov edx, len
-		mov ecx, msg
-		mov ebx, 1
-		mov eax, 4
-		int 0x80
-		jmp _start
+_start:
+	mov edx, len
+	mov ecx, msg
+	mov ebx, 1
+	mov eax, 4
+	int 0x80
+	jmp _start
 		
 section .data
-msg		db "fard",0xa
-len		equ $-msg
+	msg		db "fard",0xa
+	len		equ $-msg
